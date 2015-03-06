94ac38dbddb36b336e8a75e11a397a44a69a2a7e
Michael Ripley <zkxs00@gmail.com>
2015-03-06 01:41:59 -0600

    Rename .swfs with multiple consecutive '_'s to only have one

--- img/will.sh
+++ img/will.sh
@@ -2,6 +2,7 @@
 cd will
 rename -v 'y/A-Z/a-z/' * # upper to lower case
 rename -v 's/[ ]/_/g' * # spaces
+rename -v 's/__+/_/g' * # multiple _ characters
 rename -v "s/'//g" * # the ' character
 rename -v 's/[)(!~]//g' * # weird characters
 rename -v 's/_-_/-/g' * # the _-_ sequence that sometimes happense after space replacement
