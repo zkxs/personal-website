bec061faf68d6c282fef13fa685dce39e83fcff5
Michael Ripley <zkxs00@gmail.com>
2015-04-22 15:09:30 -0500

    Made the output more consistent and fixed a minor bug.
    Printed lines are now more consistent, being in the format 'message: offending file(s)'
    
    The minor bug that was resolved caused some sequences of multiple underbars to make it through the renaming process

--- img/will.sh
+++ img/will.sh
@@ -4,14 +4,42 @@ SCRIPTPATH="$(readlink -f "$0")"
 
 if [ "$1" == "makelink" ]; then # $2 is full path to file
 	
-	FILENAME="$(basename "$2")"
+	FILEPATH="$2"
+        FILENAME="$(basename "$2")"
 	
 	#find . -type l -exec readlink -f "{}" \; | fgrep -c "$2" # find duplicates
 	
-	ln -s "$2" "$FILENAME"
-	
+	ln -s "$FILEPATH" "$FILENAME" 2>/dev/null
+	RESULT="$?"
+	if [ "$RESULT" = "0" ]; then # no problems
+		: # do nothing
+	elif [ "$RESULT" = "1" ]; then # duplicate file
+		"$SCRIPTPATH" printdupe "$FILEPATH"
+	else # unknown
+		echo "Unknown error $RESULT when making link to $FILEPATH"
+	fi
 	
 
+elif [ "$1" == "printdupe" ]; then #2 is a file whose link already exists
+
+	FILEPATH="$2"
+        FILENAME="$(basename "$2")" # the name the link would have had
+        REALFILE1="$(readlink -f "$FILENAME")" # path to first file
+	REALFILE2="$(readlink -f "$FILEPATH")" # path to newer file
+	echo "Duplicate files: ($REALFILE1) ($REALFILE2)"
+
+	
+elif [ "$1" == "deadlink" ]; then #2 is a dead link
+	
+	LINKPATH="$2"
+	FILENAME="$(basename "$2")"
+	REALFILE="$(readlink -f "$2")"
+	
+	echo "Preexisting link $FILENAME to $REALFILE is now dead, deleting."
+	
+	rm "$2"
+	
+	
 elif [ "$1" == "cleanup" ]; then #2 is file
 	
 	# this function checks for invalid filenames
@@ -25,21 +53,29 @@ elif [ "$1" == "cleanup" ]; then #2 is file
 		RESULT=$(echo "$FILENAME" | grep -c "$1")
 		
 		if [ $RESULT -ne 0 ]; then
-			echo "$FILENAME ($REALFILE) is a duplicate, or not allowed"
+			if [ "$2" == "1" ]; then
+				echo "$FILENAME ($REALFILE) is not allowed"	
+			else
+				# this implies the rename failed because the file exists
+				"$SCRIPTPATH" printdupe "$LINKPATH"
+			fi
+			
 			rm "$LINKPATH"
 			exit 1
 		fi
 	}
 	
-	
+	# Check for disallowed characters
 	check "[A-Z]"
-	check " "
+	check '\s'
 	check "__"
 	check "'"
 	check "[)(!~]"
 	check "_-_"
-	check "iron[^a-z]*man"
-	check "flow\.swf"
+
+	# Check for disallowed files
+	check "iron[^a-z]*man" 1
+	check "flow\.swf" 1
 	
 	if [ -e "../$FILENAME" ]; then
 		if [ "$(readlink -f "../$FILENAME")" != "$(readlink -f "$FILENAME")" ]; then 
@@ -53,10 +89,10 @@ elif [ "$1" == "operate" ]; then #2 is file
 	
 	function renamingfunction {
 		rename $@ 'y/A-Z/a-z/' * # upper to lower case
-		rename $@ 's/[ ]/_/g' * # spaces
-		rename $@ 's/__+/_/g' * # multiple _ characters
+		rename $@ 's/[\s]/_/g' * # whitespace
 		rename $@ "s/'//g" * # the ' character
 		rename $@ 's/[)(!~]//g' * # weird characters
+		rename $@ 's/__+/_/g' * # multiple _ characters
 		rename $@ 's/_-_/-/g' * # the _-_ sequence that sometimes happense after space replacement
 	}
 	
@@ -64,7 +100,7 @@ elif [ "$1" == "operate" ]; then #2 is file
 	cd will
 	
 	# remove dead symlinks
-	find -L . -type l -delete
+	find -L . -type l -exec "$SCRIPTPATH" deadlink "{}" \;
 	
 	# process our trusted files first
 	renamingfunction -v
@@ -74,7 +110,7 @@ elif [ "$1" == "operate" ]; then #2 is file
 	if [ ! -d links ] ; then mkdir links ; fi
 	cd links
 		find "/home/wsmith/swf" -name "*.swf" -exec "$SCRIPTPATH" makelink "{}" \;
-		renamingfunction 2>/dev/null
+		renamingfunction #2>/dev/null
 		find . -type l -exec "$SCRIPTPATH" cleanup "{}" \;
 		find . -exec readlink -f "{}" \; | sort | uniq -d | sed 's/\(.*\)/Duplicate links to \1/'
 	cd ..
