beeb350671b81e58c71d8ee4a54f682fd822ecd9
Michael Ripley <zkxs00@gmail.com>
2015-05-17 22:40:46 -0500

    blacklisted the awful glitchy swf

--- img/will.sh
+++ img/will.sh
@@ -85,6 +85,7 @@ elif [ "$1" == "cleanup" ]; then #2 is file
 	check "bad.?apple" 1
 	check "citronnade.?flower" 1
 	check "rolling.?udonge" 1
+	check "wew12" 1
 	
 	if [ -e "../$FILENAME" ]; then
 		if [ "$(readlink -f "../$FILENAME")" != "$(readlink -f "$FILENAME")" ]; then 
